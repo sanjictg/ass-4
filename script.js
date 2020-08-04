7d9d8f66486a0400900ae1300a2d2b32a4d37a70
sanjana shahjahan <sanjanashahjahan9@gmail.com>
2020-08-04 19:10:31 +0600

    done

--- script.js
+++ script.js
@@ -4,20 +4,8 @@ submitBtn.addEventListener("click",function(){
 const notifySection = documentgetElementById("notify-section");
 notifySection.style.display = "none";
 }
-   
-
-//generate random pin
-function generate(){
-//set pin length/complexity
-let complexity =document.getElementById("slider").Value;
-//possible pin values
-let values = 123456789
-let pin = "";
-//create for loop to choose pin characters
-for( var i = 0; i <= complexity; i++){
-pin = pin+values.charAt(Math.floor(Math.random() * Math.floor(values.length-1)));
-}
-// add pin to display area
-document.getElementById("display").value = pin;
-
+for (var i= 0; i<10; i++){
+    var randomaNum = Math.floor(Math.random(max-min))+1;
+    var output = Math.round(randomNum);
+    console.log(output);
 }
